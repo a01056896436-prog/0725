6ae40bbe97f16c1857bd0d531a0bc07537684af2
a01056896436-prog <a01056896436@gmail.com>
2026-03-19 15:53:11 +0900

    ad ch03

---
+++ ch03/dataype.js
@@ -0,0 +1,23 @@
+let num = 10; 
+num = -1;
+num = 3.14;
+
+let str = "문자열";
+let str2 = "문자열" + num;
+let str3 = '문자열';
+
+let bool = true;
+let bool2 = false;
+let n = null;
+let u = undefined;
+
+let a = {
+    key: "value",
+    key2: "value2",
+};
+
+a.key="mosidied value";
+console.log(a);
+
+const arr = [1,2,3,4];
+arr[0] = 3;
